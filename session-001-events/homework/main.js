e2dbf02309bcf4f4b40ba4825a0cda1ff0c0077c
autobuz <mihaigeru94@gmail.com>
2024-07-16 14:38:39 +0300

    Made the player move and removed the spaces between the collumns

--- session-001-events/homework/main.js
+++ session-001-events/homework/main.js
@@ -7,12 +7,10 @@ const map = {
 }
 
 const player = {
-    img: '',
+    img: 'img/gorilla.svg',
     posX: 0,
     posY: 0,
 };
-
-
 
 const display = document.querySelector('.display');
 display.style.gridTemplateColumns = `repeat(${map.cols}, 1fr)`
@@ -21,12 +19,69 @@ for (let i = 0; i < map.rows; i++) {
     map.tiles[i] = [];
     for (let j = 0; j < map.cols; j++) {
         let mapTile = document.createElement('div');
-        mapTile.textContent = `${i}, ${j}`;
+        // mapTile.textContent = `${j}, ${i}`;
         mapTile.classList.add('map-tile');
         display.append(mapTile);
         map.tiles[i][j] = mapTile;
     } 
 }
 
-map.tiles[2][3].textContent = 'abc'
+map.tiles[0][0].style.backgroundImage = `url(${player.img})`;
 
+document.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'ArrowUp':
+            player.posY--;
+            if (player.posY < 0) {
+                player.posY = 0;
+                console.log("you can't go there");
+            }
+            else{
+                movePlayer(player.posY + 1, player.posX);  
+            }               
+            break;
+
+        case 'ArrowDown':
+            player.posY++;
+            if (player.posY > map.rows - 1) {
+                player.posY = map.rows - 1;
+                console.log("you can't go there");
+            }
+            else{
+                movePlayer(player.posY - 1, player.posX);  
+            }     
+            break;
+
+        case 'ArrowLeft':
+            player.posX--;
+            if (player.posX < 0) {
+                player.posX = 0;
+                console.log("you can't go there");
+            }
+            else{
+                movePlayer(player.posY, player.posX + 1);   
+            }
+            break;
+
+        case 'ArrowRight':
+            player.posX++;
+            if (player.posX > map.cols - 1) {
+                player.posX = map.cols - 1;
+                console.log("you can't go there");
+            }
+            else{
+                movePlayer(player.posY, player.posX - 1);
+            }     
+            break;
+    
+        default:
+            break;
+    }
+})
+
+function movePlayer(y, x){
+    map.tiles[y][x].style.background = 'none';    
+    map.tiles[player.posY][player.posX].style.backgroundImage = `url(${player.img})`;
+    map.tiles[player.posY][player.posX].style.backgroundSize = 'cover';
+    map.tiles[player.posY][player.posX].style.backgroundPosition = 'center';
+}
